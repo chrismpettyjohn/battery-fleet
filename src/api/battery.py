483e418542a82cd009adbe79f35a45cbe3bf83ae
habbo-hotel <chrismpettyjohn999@gmail.com>
2024-10-21 15:23:47 -0400

    feat: profit per battery

--- src/api/battery.py
+++ src/api/battery.py
@@ -5,13 +5,19 @@ class Battery:
         self.discharge_rate = discharge_rate
         self.efficiency = efficiency
         self.energy_level = 0
+        self.profit = 0
 
     def charge(self, price, hours):
         max_possible_charge = min(self.charge_rate * hours * self.efficiency, self.capacity - self.energy_level)
         self.energy_level += max_possible_charge
+        self.profit += max_possible_charge * -price  # Update profit for charging
         return max_possible_charge * -price
 
     def discharge(self, price, hours):
         max_possible_discharge = min(self.discharge_rate * hours, self.energy_level)
         self.energy_level -= max_possible_discharge
+        self.profit += max_possible_discharge * price  # Update profit for discharging
         return max_possible_discharge * price
+
+    def calculate_profit(self):
+        return self.profit  # Return the current profit
